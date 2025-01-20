04d4dcb0ab75592295e41c1bfd26886a25938420
Akshay1114 <sharma41114@gmail.com>
2025-01-20 13:11:07 -0800

    refactor:navbar

--- src/components/naviagtion/Navbar.jsx
+++ src/components/naviagtion/Navbar.jsx
@@ -1,11 +1,27 @@
-import React from 'react'
+import React from "react";
+import { Link } from "react-router-dom";
 
 function Navbar() {
   return (
     <div>
-      Navbar
+      <ul className="navbar">
+        <li>
+          <Link class="active" href="#home">
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link href="#news">News</Link>
+        </li>
+        <li>
+          <Link href="#contact">Contact</Link>
+        </li>
+        <li>
+          <Link href="#about">About</Link>
+        </li>
+      </ul>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
