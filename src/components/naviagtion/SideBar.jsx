9077efe42329fecabefc882cb19b90c7afa1df4b
PsYcO-Jass <jsingh354@mylangara.ca>
2025-02-20 15:27:52 -0800

    Stable the Dshboard enviornment

--- src/components/naviagtion/SideBar.jsx
+++ src/components/naviagtion/SideBar.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
+import { Outlet } from 'react-router-dom'
 
 function SideBar() {
   return (
     <div>
-      SideBar
+      <Outlet/>
     </div>
   )
 }
--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -43,7 +43,7 @@ const router = createBrowserRouter([
     
     },
     {
-      element:<Sidebar/>,
+      element:<Navbar/>,
       errorElement: <ErrorPage />,
       children: [
       {
