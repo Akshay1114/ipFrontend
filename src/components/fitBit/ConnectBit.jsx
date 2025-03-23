d28824246b92faeb86edaa6e94d2ef839d545f0f
Akshay1114 <sharma41114@gmail.com>
2025-03-23 14:19:19 -0700

    domain changed

--- src/components/fitBit/ConnectBit.jsx
+++ src/components/fitBit/ConnectBit.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 function ConnectBit() {
     const CLIENT_ID = "23Q4W9";
-    const REDIRECT_URI = "https://ip-frontend-pi.vercel.app/callback";
+    const REDIRECT_URI = "https://wingwise.org/callback";
     const AUTH_URL = `https://www.fitbit.com/oauth2/authorize?response_type=code&client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&scope=sleep%20heartrate&expires_in=604800`;
   return (
     <div className="ConnectBit">
